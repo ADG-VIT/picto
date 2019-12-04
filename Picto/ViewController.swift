27c2a03ee9bb6c0b8c7ee31ef4bfce73c462b02e
tushar1210 <ts26192271@gmail.com>
2019-12-04 22:02:44 +0530

    Initial

---
+++ Picto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picto
+//
+//  Created by Tushar Singh on 04/12/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
